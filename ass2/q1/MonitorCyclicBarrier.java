c815e132e6b1c2c5e63b94245d596f2ca06c0ca8
rochellev <rochellevroberts@gmail.com>
2017-02-09 16:40:05 -0600

    monitor implementation has the same behavior of semaphore now

--- ass2/q1/MonitorCyclicBarrier.java
+++ ass2/q1/MonitorCyclicBarrier.java
@@ -15,18 +15,17 @@ public class MonitorCyclicBarrier {
 	public MonitorCyclicBarrier(int parties){
 		this.parties = parties;
 		this.count = 0;
-		
 	}
 	
 	
 	public synchronized int await() throws InterruptedException {
-		
 		count++;
 		int index = parties - count;
-		if(count != parties){
-			
+		if(count == parties){     // or is it count == parties -1 idk
+			count = 0; //reset
+			this.notifyAll();  //want to release threads
 		}else{
-			count = 0; //time to reset
+			this.wait(); //need to wait for more threads
 		}
 		
 		return index;
