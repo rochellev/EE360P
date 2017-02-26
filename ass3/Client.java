cef8c975f5dc2c965840d226ece2eac8e4d8bc25
rochellev <rochellevroberts@gmail.com>
2017-02-25 19:06:23 -0600

    setmode logic done, needs to be tested

--- ass3/Client.java
+++ ass3/Client.java
@@ -21,13 +21,21 @@ public class Client {
     udpPort = Integer.parseInt(args[2]);
 
     Scanner sc = new Scanner(System.in);
+    boolean modeFlag; //flag default TCP = false and UDP = true
     while(sc.hasNextLine()) {
       String cmd = sc.nextLine();
       String[] tokens = cmd.split(" ");
-
       if (tokens[0].equals("setmode")) {
         // TODO: set the mode of communication for sending commands to the server 
         // and display the name of the protocol that will be used in future
+    	  if(tokens.length == 2 && tokens[1].equals("U")){
+    		  System.out.println("Protocol set to UDP");
+    		  modeFlag = true;
+    	  }else{
+    		  System.out.println("Protocol set to TCP");
+    		  modeFlag = false;  // set default
+    	  }
+    	  
       }
       else if (tokens[0].equals("purchase")) {
         // TODO: send appropriate command to the server and display the
