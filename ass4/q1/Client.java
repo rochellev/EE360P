0d4c16831a706b2fbff7659c51b5eda9dd6cb925
rochellev <rochellevroberts@gmail.com>
2017-03-21 10:45:01 -0500

    started working...

--- ass4/q1/Client.java
+++ ass4/q1/Client.java
@@ -1,35 +1,41 @@
 package q1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
-  public static void main (String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int numServer = sc.nextInt();
-    
-    for (int i = 0; i < numServer; i++) {
-      // TODO: parse inputs to get the ips and ports of servers
-    }
 
-    while(sc.hasNextLine()) {
-      String cmd = sc.nextLine();
-      String[] tokens = cmd.split(" ");
+	private static ArrayList<String> serverList;
 
-      if (tokens[0].equals("purchase")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("cancel")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("search")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("list")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else {
-        System.out.println("ERROR: No such command");
-      }
-    }
-  }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numServer = sc.nextInt();
+
+		for (int i = 0; i < numServer; i++) {
+			// TODO: parse inputs to get the ips and ports of servers
+			String serverIPS = sc.nextLine();  //should be id? 
+			serverList.add(serverIPS);
+		}
+
+		while (sc.hasNextLine()) {
+			String cmd = sc.nextLine();
+			String[] tokens = cmd.split(" ");
+
+			if (tokens[0].equals("purchase")) {
+				// TODO: send appropriate command to the server and display the
+				// appropriate responses form the server
+			} else if (tokens[0].equals("cancel")) {
+				// TODO: send appropriate command to the server and display the
+				// appropriate responses form the server
+			} else if (tokens[0].equals("search")) {
+				// TODO: send appropriate command to the server and display the
+				// appropriate responses form the server
+			} else if (tokens[0].equals("list")) {
+				// TODO: send appropriate command to the server and display the
+				// appropriate responses form the server
+			} else {
+				System.out.println("ERROR: No such command");
+			}
+		}
+	}
 }
